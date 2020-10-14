8ff853c4a946e26dda9ed2207d9f8ca1f4a56880
veerup <veebox@mail.ru>
2020-10-14 18:35:52 +0300

    установлены overtrue/phplint, squizlabs/php_codesniffer, yii2-scheduling + приведен код в соответствии с PSR-12

--- console/migrations/m190124_110200_add_verification_token_column_to_user_table.php
+++ console/migrations/m190124_110200_add_verification_token_column_to_user_table.php
@@ -1,14 +1,20 @@
 <?php
 
-use \yii\db\Migration;
+use yii\db\Migration;
 
 class m190124_110200_add_verification_token_column_to_user_table extends Migration
 {
+    /**
+     * {@inheritDoc}
+     */
     public function up()
     {
         $this->addColumn('{{%user}}', 'verification_token', $this->string()->defaultValue(null));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function down()
     {
         $this->dropColumn('{{%user}}', 'verification_token');
